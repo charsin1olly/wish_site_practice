12387c4e11cb8cbb9ca9624764d2435ec8fd859b
Charsin Chan <116936412+charsin1olly@users.noreply.github.com>
2022-11-17 21:37:10 +0800

    Ｃ完成

--- app/controllers/wish_lists_controller.rb
+++ app/controllers/wish_lists_controller.rb
@@ -1,3 +1,23 @@
 class WishListsController < ApplicationController
  
+  def index
+  end
+
+  def new
+    @wish_list = WishList.new
+  end
+
+  def create
+    @wish_list = WishList.new(clean_wish_param)
+    if(@wish_list.save)
+      redirect_to root_path ,notice: "新增成功！"
+    else
+      render :new
+    end
+  end
+
+  private
+  def clean_wish_param
+    params.require(:wish_list).permit(:title , :content)
+  end
 end
