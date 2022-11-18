47ba9590bd70697db228e5f78e14eebdc79ee95b
Charsin Chan <116936412+charsin1olly@users.noreply.github.com>
2022-11-19 01:00:12 +0800

    UD

--- app/controllers/wish_lists_controller.rb
+++ app/controllers/wish_lists_controller.rb
@@ -9,7 +9,7 @@ class WishListsController < ApplicationController
   end
 
   def create
-    @wish_list = WishList.new(clean_wish_param)
+    @wish_list = WishList.new(clean_wish_params)
     if(@wish_list.save)
       redirect_to root_path ,notice: "新增成功！"
     else
@@ -18,11 +18,35 @@ class WishListsController < ApplicationController
   end
 
   def show
-    @wish_list = WishList.find(params[:id])
+    find_wish
+  end
+
+  def edit
+    find_wish
+  end
+
+  def update
+    find_wish
+    if @wish_list.update(clean_wish_params)
+      redirect_to root_path , notice: "成功更新許願卡！"
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    find_wish
+    @wish_list.destroy
+    redirect_to root_path, notice: "資料已刪除"
   end
 
   private
-  def clean_wish_param
-    params.require(:wish_list).permit(:title , :content)
+  def clean_wish_params
+    params.require(:wish_list).permit(:title, :content)
+  end
+
+  def find_wish
+    @wish_list = WishList.find(params[:id])
   end
 end
